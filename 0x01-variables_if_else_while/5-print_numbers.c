e76a5a957258a1b8f07b620eb211b1660a211993
petersembi <petersembi@gmail.com>
2022-12-31 01:24:51 -0800

    task 5 done

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+/* more headers goes there */
+
+
+
+/**
+*main - Entry point
+*Return: zero(success)
+*/
+
+int main(void)
+
+{
+	int i;
+
+	for  (i = 0; i <= 9; i++)
+	{
+		printf ("%d", i);
+	}
+
+	putchar ('\n');
+	return (0);
+}
