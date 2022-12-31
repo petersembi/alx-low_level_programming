117995cf9e54043d32cb5fcedd750afdfc07a572
petersembi <petersembi@gmail.com>
2022-12-31 08:00:38 -0800

    task-0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+/* more headers goes there */
+
+
+
+/**
+*main - Entry point
+*Return: zero(success)
+*/
+
+int main(void)
+
+{
+	printf ("_putchar\n");
+	return (0);
+}
