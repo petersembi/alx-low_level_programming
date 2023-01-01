4a42741aa5b829e62a63b92bfa5ff8ccbec2b6f1
petersembi <petersembi@gmail.com>
2023-01-01 09:30:19 -0800

    task 1 done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+#include "main.h"
+
+/* more headers goes there */
+
+
+
+/**
+*main - Entry point
+*Return: zero(success)
+*/
+
+int main(void)
+
+{
+	print_alphabet();
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,13 @@
 int _putchar(char c);
+
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar (ch);
+	}
+
+	putchar('\n');
+}
