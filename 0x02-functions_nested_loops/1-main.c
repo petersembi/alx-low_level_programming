651ef1078d2b0b9730e8a633aaa568aac30c233f
petersembi <petersembi@gmail.com>
2023-01-01 10:50:05 -0800

    m

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <unistd.h>
+
+#include "main.h"
+
+
+
+/**
+*main - Entry point
+*Return: zero(success)
+*/
+
+
+
+int main(void)
+
+{
+	print_alphabet();
+	return (0);
+}
