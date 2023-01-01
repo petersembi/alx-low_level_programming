c36d0d59e00e847081d734cec9656cf3d01b7df3
petersembi <petersembi@gmail.com>
2023-01-01 09:04:32 -0800

    done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,16 +10,6 @@
 
 
 
-/* more headers goes there */
-
-/**
-*_putchar -writes the character c to stdout
-*@c: The character to print
-*
-* Retrun: On success 1. 
-* On error, -1 is returned, and errno is set appropriately. 
-*/
-
 /**
 *main - Entry point
 *Return: zero(success)
